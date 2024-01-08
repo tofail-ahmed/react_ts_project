d20782c2f505e6a39c0aebd9a6d5bfa8716f21ea
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-08 16:52:39 +0600

    form added

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,34 +1,66 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Form = () => {
-      const [formData, setFormData] = useState({
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-          });
+  const [user, setUser] = useState({
+    name: "user",
+    email: "user@example.com",
+    password: "password",
+  });
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
+
   return (
-    <form className="bg-slate-600 p-4 mx-auto flex flex-col justify-center gap-4 my-4 rounded-xl">
-      <div className="">
-        <label>
-          Username:
-          <input type="text" name="name" />
-        </label>
-      </div>
-      <div>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-      </div>
-      <div>
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    <div>
+      {submitted ? (
+        <div className="bg-slate-600 p-4 mx-auto flex flex-col justify-center gap-4 my-4 rounded-xl">
+          <h2>User Details:</h2>
+          <h3>Name: {user.name}</h3>
+          <h3>Email: {user.email}</h3>
+          <h3>Password: {user.password}</h3>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-600 p-4 mx-auto flex flex-col justify-center gap-4 my-4 rounded-xl"
+        >
+          <div className="">
+            <label>
+              Username:
+              <input
+                onChange={(e) => setUser({ ...user, name: e.target.value })}
+                type="text"
+                name="name"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Email:
+              <input
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                type="email"
+                name="email"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password:
+              <input
+                 onChange={(e) => setUser({...user,password: e.target.value})}
+                type="password"
+                name="password"
+              />
+            </label>
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      )}
+    </div>
   );
 };
 
