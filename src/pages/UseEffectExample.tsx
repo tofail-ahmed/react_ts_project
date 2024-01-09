9620fbb33d9a4fa422e50169a8798157f8abb5dc
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-10 00:23:57 +0600

    more useEffect

--- src/pages/UseEffectExample.tsx
+++ src/pages/UseEffectExample.tsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react";
 
 function UseEffectExample() {
   const [hidden, sethidden] = useState(false);
-
+  const [user,setUser]=useState({name:"",email:""})
+console.log(user)
   useEffect(() => {
-    console.log("render");
+//     console.log("render");
     return () => {
-      console.log("Cleanup");
+      // console.log("Cleanup");
     };
   }, [hidden]);
-
+useEffect(()=>{
+console.log("Render")
+},[user.name,user.email])
   const conditionalBg = {
     backgroundColor: hidden ? "red" : "green",
   };
@@ -17,7 +20,11 @@ function UseEffectExample() {
   return (
     <div style={conditionalBg}>
       <div>
-        <h3 className="text-center">UseEffectExample</h3>
+            <input onBlur={(e)=>setUser({...user,name:e.target.value})} type="text" name="name" id="name" className="border border-red-800" placeholder="name" />
+            <input onBlur={(e)=>setUser({...user,email:e.target.value})} type="text" name="email" id="email" className="border border-red-800" placeholder="email" />
+      </div>
+      <div>
+        {/* <h3 className="text-center">UseEffectExample</h3> */}
 
         {hidden ? (
           <button onClick={() => sethidden(!hidden)}>Unhide</button>
@@ -34,7 +41,8 @@ function UseEffectExample() {
       <div className="bg-slate-400 border border-orange-400">
         <h3>Interval Counter</h3>
         <h1>{/* {!hidden&&<p>{count}</p>} */}</h1>
-        {!hidden&& <Counter/>}
+        {/* {!hidden&& <Counter/>} */}
+        {/* {!hidden&& <Todos/>} */}
       </div>
     </div>
   );
@@ -43,7 +51,7 @@ const Counter = () => {
   const [count, setCount] = useState(0);
   useEffect(() => {
     const intervalID = setInterval(() => {
-      console.log("count")
+      // console.log("count")
       setCount((prev) => prev + 1);
     }, 1000);
     return () => {
@@ -53,5 +61,26 @@ const Counter = () => {
   return <h1>{count}</h1>;
 };
 
+const Todos = () => {
+      const [todos,setTodos]=useState();
+      const controller=new AbortController();
+      const signal=controller.signal;
+      useEffect(()=>{
+            fetch("https://jsonplaceholder.typicode.com/todos",{signal}).then(res=>res.json()).then(data=>setTodos(data) )
+            return ()=>{
+                  controller.abort()
+            }
+      },[])
+      console.log(todos)
+  return <div>  
+      <ul>
+        {todos&&todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+   
+    </div>;
+ 
+};
 
 export default UseEffectExample;
